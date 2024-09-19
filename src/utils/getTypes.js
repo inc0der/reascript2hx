2da851274377643b55610489bc8b8479a9eedde2
inc0der <inc0der.dev@gmail.com>
2024-09-18 21:02:05 -0300

    add common types to exclude from type extern generation

--- src/utils/getTypes.js
+++ src/utils/getTypes.js
@@ -5,18 +5,20 @@ function getTypes(ast) {
 
   const types = new Map();
 
+  const commonTypesToExclude = ['integer', 'number', 'string', 'boolean'];
+
   traverseFields(reaper, (field) => {
 
     if (field.params) {
       for (const param of field.params) {
-         if (!param.type) continue
+         if (!param.type || commonTypesToExclude.includes(param.type)) continue
         types.set(param.type, param.type)
       }
     }
 
     if (field.returns) {
       for (const ret of field.returns) {
-         if (!ret.type) continue
+         if (!ret.type || commonTypesToExclude.includes(ret.type)) continue
         types.set(ret.type, ret.type)
       }
     }
@@ -25,14 +27,14 @@ function getTypes(ast) {
   traverseFields(gfx, (field) => {
     if (field.params) {
       for (const param of field.params) {
-        if (!param.type) continue
+        if (!param.type || commonTypesToExclude.includes(param.type)) continue
         types.set(param.type, param.type)
       }
     }
 
     if (field.returns) {
       for (const ret of field.returns) {
-        if (!ret.type) continue
+        if (!ret.type || commonTypesToExclude.includes(ret.type)) continue
         types.set(ret.type, ret.type)
       }
     }
