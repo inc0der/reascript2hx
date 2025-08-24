720c8ce6a543cf96c08a2300ffe43be506e00958
inc0der <inc0der.dev@gmail.com>
2025-08-24 01:16:23 -0300

    feature enchance  type determination to support EitherType and special cases

--- src/utils/createHaxeFunction.js
+++ src/utils/createHaxeFunction.js
@@ -6,7 +6,27 @@ import camelcase from "camelcase";
 import { enhancedCamelCase } from "./enhancedCamelCase.js";
 import { haxeReservedKeywords } from "./haxeReserved.js";
 
-function determineType (allTypes = [], type, name) {
+function determineType(allTypes = [], type, name) {
+  if (type.includes('|')) {
+    const types = type.split('|').map(t => t.trim());
+    const convertedTypes = types.map(t => determineType(allTypes, t, name));
+    
+    if (convertedTypes.length === 2) {
+      return `haxe.extern.EitherType<${convertedTypes[0]}, ${convertedTypes[1]}>`;
+    }
+    
+    let result = convertedTypes[0];
+    for (let i = 1; i < convertedTypes.length; i++) {
+      result = `haxe.extern.EitherType<${result}, ${convertedTypes[i]}>`;
+    }
+    return result;
+  }
+
+  // NOTE: Handle special case ( ReaProject|nil|0 ) assuming 0 is an int?
+  if (!isNaN(type)) {
+    return "Int";
+  }
+
   switch (type) {
     case "integer":
       return "Int";
@@ -16,6 +36,9 @@ function determineType (allTypes = [], type, name) {
       return "Bool";
     case "string":
       return "String";
+    case "nil":
+      return "Void";
+
     default:
       if (allTypes.has(type)) {
         return allTypes.get(type);
@@ -23,19 +46,15 @@ function determineType (allTypes = [], type, name) {
       if (name && commonIntTypes.includes(name)) {
         return "Float";
       }
-
       if (name && commonStringTypes.includes(name)) {
         return "String";
       }
-
       if (name && commonBoolTypes.includes(name)) {
         return "Bool";
       }
-
       if (name && name === "function") {
         return "() -> Void";
       }
-
       return "Dynamic";
   }
 }
